5484496d54e149d4402039926a12730a7b744e5d
Sai Paresh <sai.paresh26@gmail.com>
2020-07-27 19:01:04 +0530

    First Commit

---
+++ src/com/saiparesh/Dog.java
@@ -0,0 +1,11 @@
+package com.saiparesh;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Dog {
+	public static int count = 0;
+	
+	public static void incrementCounter() {
+		count++;
+	}
+}
---
+++ src/com/saiparesh/DogTester.java
@@ -0,0 +1,25 @@
+package com.saiparesh;
+
+public class DogTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog husky = new Dog();
+		System.out.println(husky.count); //warning
+		System.out.println(Dog.count);
+		
+		Dog anotherDog = new Dog();
+		anotherDog.incrementCounter(); //warning No need for instance variable
+		
+		Dog.incrementCounter();
+		System.out.println(Dog.count);
+		System.out.println(husky.count);
+		
+		husky  = null;
+		anotherDog = null;
+		
+		System.out.println(husky.count);
+		System.out.println(anotherDog.count);
+	}
+
+}
